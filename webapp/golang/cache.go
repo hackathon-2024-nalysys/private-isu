f487f2de010709f751c4cbe881c67d66dcbe3b4b
grainrigi <naonaokiryu2@gmail.com>
2024-11-04 11:24:44 +0900

    Refactor Makefile to add proto command for generating Go code from cache.proto

--- webapp/golang/cache.go
+++ webapp/golang/cache.go
@@ -1,13 +1,14 @@
 package main
 
 import (
-	"bytes"
-	"encoding/gob"
 	"strconv"
 	"strings"
 
 	"github.com/bradfitz/gomemcache/memcache"
+	"github.com/catatsuy/private-isu/webapp/golang/grpc"
 	"github.com/catatsuy/private-isu/webapp/golang/types"
+	"google.golang.org/protobuf/proto"
+	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
 // returns hit users and missed ids
@@ -25,16 +26,20 @@ func getUsers(ids []int) (map[int]types.User, []int, error) {
 
 	hits := make(map[int]types.User, len(hitsRaw))
 
-	// decode using gob
-	var buffer bytes.Buffer
+	// decode using grpc
 	for _, hitRaw := range hitsRaw {
-		dec := gob.NewDecoder(&buffer)
-		buffer.Write(hitRaw.Value)
-		var hit types.User
-		if err := dec.Decode(&hit); err != nil {
+		var user grpc.User
+		err := proto.Unmarshal(hitRaw.Value, &user)
+		if err != nil {
 			return nil, nil, err
 		}
-		hits[hit.ID] = hit
+		hits[int(user.ID)] = types.User{
+			ID:          int(user.ID),
+			AccountName: user.AccountName,
+			Authority:   int(user.Authority),
+			DelFlg:      int(user.DelFlg),
+			CreatedAt:   user.CreatedAt.AsTime(),
+		}
 	}
 
 	missed := make([]int, 0, len(ids)-len(hits))
@@ -63,14 +68,27 @@ func getCommentsForPosts(postIDs []int) (map[int][]types.Comment, []int, error) 
 
 	hits := make(map[int][]types.Comment, len(hitsRaw))
 
-	// decode using gob
-	var buffer bytes.Buffer
+	// decode using grpc
 	for _, hitRaw := range hitsRaw {
-		dec := gob.NewDecoder(&buffer)
-		buffer.Write(hitRaw.Value)
-		var hit []types.Comment
-		if err := dec.Decode(&hit); err != nil {
+		var comments grpc.Comments
+		err := proto.Unmarshal(hitRaw.Value, &comments)
+		if err != nil {
 			return nil, nil, err
+		}
+		hit := make([]types.Comment, len(comments.Comments))
+		for i, comment := range comments.Comments {
+			hit[i] = types.Comment{
+				ID:        int(comment.ID),
+				PostID:    int(comment.PostID),
+				UserID:    int(comment.UserID),
+				Comment:   comment.Comment,
+				CreatedAt: comment.CreatedAt.AsTime(),
+				User: types.User{
+					ID:          int(comment.User.ID),
+					AccountName: comment.User.AccountName,
+					Authority:   int(comment.User.Authority),
+				},
+			}
 		}
 		id, _ := strconv.Atoi(strings.Split(hitRaw.Key, ":")[1])
 		hits[id] = hit
@@ -89,14 +107,19 @@ func getCommentsForPosts(postIDs []int) (map[int][]types.Comment, []int, error) 
 
 func cacheUsers(users []types.User) error {
 	for _, user := range users {
-		var buffer bytes.Buffer
-		enc := gob.NewEncoder(&buffer)
-		if err := enc.Encode(user); err != nil {
+		pb, err := proto.Marshal(&grpc.User{
+			ID:          int32(user.ID),
+			AccountName: user.AccountName,
+			Authority:   int32(user.Authority),
+			DelFlg:      int32(user.DelFlg),
+			CreatedAt:   timestamppb.New(user.CreatedAt),
+		})
+		if err != nil {
 			return err
 		}
 		if err := memcacheClient.Set(&memcache.Item{
 			Key:   "user:" + strconv.Itoa(user.ID),
-			Value: buffer.Bytes(),
+			Value: pb,
 		}); err != nil {
 			return err
 		}
@@ -105,14 +128,32 @@ func cacheUsers(users []types.User) error {
 }
 
 func cacheCommentsForPosts(postID int, comments []types.Comment) error {
-	var buffer bytes.Buffer
-	enc := gob.NewEncoder(&buffer)
-	if err := enc.Encode(comments); err != nil {
+	gc := grpc.Comments{
+		Comments: make([]*grpc.Comment, len(comments)),
+	}
+
+	for i, comment := range comments {
+		gc.Comments[i] = &grpc.Comment{
+			ID:        int32(comment.ID),
+			PostID:    int32(comment.PostID),
+			UserID:    int32(comment.UserID),
+			Comment:   comment.Comment,
+			CreatedAt: timestamppb.New(comment.CreatedAt),
+			User: &grpc.User{
+				ID:          int32(comment.User.ID),
+				AccountName: comment.User.AccountName,
+				Authority:   int32(comment.User.Authority),
+			},
+		}
+	}
+
+	pb, err := proto.Marshal(&gc)
+	if err != nil {
 		return err
 	}
 	if err := memcacheClient.Set(&memcache.Item{
 		Key:   "post:" + strconv.Itoa(postID),
-		Value: buffer.Bytes(),
+		Value: pb,
 	}); err != nil {
 		return err
 	}
--- webapp/golang/go.mod
+++ webapp/golang/go.mod
@@ -17,4 +17,11 @@ require (
 	github.com/gorilla/securecookie v1.1.2 // indirect
 	github.com/memcachier/mc/v3 v3.0.3 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	golang.org/x/net v0.30.0 // indirect
+	golang.org/x/sys v0.26.0 // indirect
+	golang.org/x/text v0.19.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20241021214115-324edc3d5d38 // indirect
+	google.golang.org/grpc v1.67.1 // indirect
+	google.golang.org/grpc/cmd/protoc-gen-go-grpc v1.5.1 // indirect
+	google.golang.org/protobuf v1.35.1 // indirect
 )
